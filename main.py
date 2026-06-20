fb299516e9c160aafed09c7c07a4ac78bd5d1f8c
montylr2722l <vishu.869600@gmail.com>
2026-06-20 21:16:08 +0530

    added conversation mode with timeout

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 from modules.listen import listen
 from modules.speak import speak
-from modules.greet import greet
 from modules.commands import execute
+from modules.greet import greet
+
+import time
 
 greet()
 
+conversation_mode = False
+last_activity = 0
+
 while True:
 
-    wake_word = listen()
-
-    if "exit" in wake_word:
-        speak("Goodbye Vishnu")
-        break
-
-    if "jarvis" in wake_word:
-
-        speak("Yes Vishnu")
+    if conversation_mode:
 
         command = listen()
 
         if command:
+
+            last_activity = time.time()
 
             if "exit" in command:
                 speak("Goodbye Vishnu")
                 break
 
             execute(command)
+
+        if time.time() - last_activity > 30:
+
+            conversation_mode = False
+
+            speak("Standing by")
+
+    else:
+
+        wake_word = listen()
+
+        if "exit" in wake_word:
+
+            speak("Goodbye Vishnu")
+            break
+
+        if "jarvis" in wake_word:
+
+            speak("Yes Vishnu")
+
+            conversation_mode = True
+
+            last_activity = time.time()
